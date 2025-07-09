57bc5066041cc63cf15720e8ab0e004bc5e11cb5
Hitesh Rathi <97667730+HiteshRathi02@users.noreply.github.com>
2025-07-09 09:46:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
+const romanMap = [
+  ['M', 1000],
+  ['CM', 900],
+  ['D', 500],
+  ['CD', 400],
+  ['C', 100],
+  ['XC', 90],
+  ['L', 50],
+  ['XL', 40],
+  ['X', 10],
+  ['IX', 9],
+  ['V', 5],
+  ['IV', 4],
+  ['I', 1]
+]
+
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
-
-  //your code here
-
+  if(num>1999 || num < 1) {
+    return 'Input must be between 1 and 1999';
+  }
+  let roman = '';
+  for(let i=0; i<romanMap.length; i++) {
+    let [romanChar, numToConvert] = romanMap[i];
+    while(numToConvert <= num) {
+      roman += romanChar;
+      num -= numToConvert;
+    }
+  }
+  return roman;
 }
+
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
 // console.log(convertToRoman(36));
